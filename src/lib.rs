b916e239a7f718ea78591e17e2d8997172f86d73
daleondev <david@rodenheber.de>
2025-04-27 14:39:24 +0200

    fix lib

--- src/lib.rs
+++ src/lib.rs
@@ -4,33 +4,11 @@ pub mod imu_interface_ffi;
 pub mod hardware;
 pub mod serial_interface;
 
-// #[cfg(test)]
-// mod tests {
+#[cfg(test)]
+mod tests {
 
-//     #[test]
-//     fn it_works() {
+    #[test]
+    fn it_works() {
         
-//     }
-// }
-
-// #[repr(C)]
-// #[derive(Debug, Copy, Clone, PartialEq, Eq)]
-// pub enum ImuError {
-//     ImuSuccess = 0,
-//     ImuErrorAlreadyInit,
-//     ImuErrorNotInit,
-//     ImuErrorInitFailed,
-//     ImuErrorNoDevicesFound,
-//     ImuErrorInterfaceAlreadyExists,
-//     ImuErrorInterfaceDoesntExist,
-//     ImuErrorOpenFailed,
-//     ImuErrorAlreadyRunning,
-//     ImuErrorNotRunning,
-//     ImuErrorUnknown,
-// }
-
-// #[unsafe(no_mangle)]
-// pub extern "C" fn init_imu() -> ImuError
-// {
-//     ImuError::ImuSuccess
-// }
+    }
+}
